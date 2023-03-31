64d0c3b1deb21c947a547f6ac40460d968134d8a
LobotomyTerror <128017685+LobotomyTerror@users.noreply.github.com>
2023-03-31 15:36:24 -0600

    Update main.cpp

--- BFS/main.cpp
+++ BFS/main.cpp
@@ -1,282 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
-
-template<typename T>
-class Node
-{
-public:
-    Node<T>* next;
-    T data;
-    
-    Node();
-    Node(T);
-};
-
-template<typename T>
-Node<T>::Node()
-{
-    next  = nullptr;
-}
-
-template<typename T>
-Node<T>::Node(T d)
-{
-    this->data = d;
-    next  = nullptr;
-}
-
-template<typename T>
-class Graph : public Node<T>
-{
-private:
-    int numOfVertices, numOfEdges, size;
-    Node<T>* head;
-    Node<T>* tail;
-    Node<T>** adj;
-    
-public:
-    Graph();
-    ~Graph();
-    void initGraph(int V);
-    void createLink(T, T);
-    void print(T, T, T, T);
-    void BFS(T, int[], int[], int[]);
-    void enqueue(T);
-    void dequeue();
-    int getSize();
-    Node<T>* getData();
-};
-
-template<typename T>
-Graph<T>::Graph()
-{
-    head = tail = nullptr;
-    size = 0;
-}
-
-template<typename T>
-Graph<T>::~Graph()
-{
-    for (int i = 0; i < numOfVertices; ++i)
-        delete adj[i];
-    delete adj;
-}
-
-template<typename T>
-void Graph<T>::initGraph(int V)
-{
-    numOfVertices = V;
-    numOfEdges = 0;
-    adj = new Node<T>*[V];
-    for (int i = 0; i < numOfVertices; ++i)
-    {
-        adj[i] = new Node<T>();
-        adj[i] = nullptr;
-    }
-}
-
-template<typename T>
-void Graph<T>::createLink(T v, T e)
-{
-    Node<T>* temp1 = new Node<T>(v);
-    Node<T>* temp2 = new Node<T>(e);
-    Node<T>* last_node;
-    Node<T>* first_node;
-    
-    if (temp1->data == temp2->data)
-    {
-        temp1 = temp2;
-        adj[v] = temp1;
-        ++numOfVertices;
-    }
-    else if (adj[v] == nullptr)
-    {
-        first_node = temp1;
-        last_node = temp2;
-        first_node->next = last_node;
-        adj[v] = first_node;
-        ++numOfVertices;
-    }
-    else
-    {
-        first_node = adj[v];
-        last_node = first_node->next;
-        while (last_node->next != nullptr)
-            last_node = last_node->next;
-        last_node->next = temp2;
-        adj[v] = first_node;
-    }
-    ++numOfEdges;
-}
-
-template<typename T>
-int Graph<T>::getSize()
-{
-    return size;
-}
-
-template<typename T>
-void Graph<T>::enqueue(T d)
-{
-    Node<T>* new_node = new Node(d);
-    if (tail == nullptr)
-    {
-        tail = head = new_node;
-    }
-    else
-    {
-        tail->next = new_node;
-        tail = new_node;
-    }
-    ++size;
-}
-
-template<typename T>
-void Graph<T>::dequeue()
-{
-    Node<T>* temp = head;
-    
-    if (head == nullptr)
-        return;
-    
-    head = head->next;
-    
-    if (head == nullptr)
-        tail = nullptr;
-    
-    --size;
-    delete temp;
-}
-
-template<typename T>
-Node<T>* Graph<T>::getData()
-{
-    return this->head;
-}
-
-//Breadth-first search to find the shortest path from a
-//user-defined source node to a destination node...
-template<typename T>
-void Graph<T>::BFS(T src, int pathTo[], int destTo[], int pred[])
-/*For this to work we need to set a pathTo array, distTo array and an index array
-to store the path from the source node to the destination node
-along with the dist to array to store the edges from the two nodes
-then an index to have the starting node and destination node in the same array to
-loop back through and find the path from dest to src, then we flip it to show the details.*/
-{
-    int i = 0;
-    bool vistedV[numOfVertices];
-    Node<T>* next_node;
-    Node<T>* curr;
-    Node<T>* tempArr[numOfVertices];
-    
-    for (int i = 0; i < numOfVertices; ++i)
-    {
-        vistedV[i] = false;
-        tempArr[i] = adj[i];
-    }
-    
-    vistedV[src] = true;
-    next_node = curr = tempArr[src];
-    next_node = next_node->next;
-    pathTo[i] = curr->data;
-    destTo[curr->data] = 0;
-    pred[i] = -1;
-    enqueue(curr->data);
-    
-    while (getSize() != 0)
-    {
-        while (next_node != nullptr)
-        {
-            if (vistedV[next_node->data] == false)
-            {
-                ++i;
-                pathTo[i] = next_node->data;                                   //stores the node we are currently at
-                pred[i] = curr->data;                                               //predecessor to the node we are currently at
-                destTo[next_node->data] = destTo[curr->data] + 1; //sets the arr element to the number edges from the src node
-                vistedV[next_node->data] = true;                           //just a check if the node has already been visted
-                enqueue(next_node->data);
-            }
-            next_node = next_node->next;  //advance the node keeping it moving so we
-                                                            //can check the if there is any more links to the curr node
-        }
-        dequeue();
-        if ((next_node = getData()) != nullptr) //this gets the data from the head element such that if
-                                                                    //it is not NULL then we can advance the pointers
-        {
-            next_node = curr = tempArr[next_node->data];  //as mentioned above also it stops from any bad_access issues
-                                                                                    //if the element in the array goes out of bounds
-            next_node = next_node->next;
-        }
-    }
-}
-
-template<typename T>
-void Graph<T>::print(T src, T dest_one, T dest_two, T dest_three)
-{
-    //three arrays pathTo, pred, and distTo
-    //these three will be used to store each node,
-    //which node is a predecessor of that node, and
-    //the number of edges from the src node
-    
-    //three array declrations
-    int pathTo[numOfVertices];
-    int distTo[numOfVertices];
-    int pred[numOfVertices];
-    
-    for (int i = 0; i < numOfVertices; ++i) //init of the three arrays
-    {
-        pathTo[i] = 0;
-        distTo[i] = 0;
-        pred[i] = 0;
-    }
-    BFS(src, pathTo, distTo, pred);
-    
-    for (int i = 0; i < numOfVertices; ++i)
-    {
-        if (pathTo[i] == dest_one || pathTo[i] == dest_two || pathTo[i] == dest_three)
-        {
-            std::cout << pathTo[i] << ' ';
-            int pnode = pred[i];
-            int cnode = pathTo[i];
-//            int dnode = distTo[i];
-            //int tempArr[i];
-            //tempArr[i] = cnode;
-            
-            for (int j = i; j >= 0; --j)
-            {
-                if (pathTo[j] == pnode)
-                {
-                    std::cout << pathTo[j] << ' ';
-                    pnode = pred[j];
-                    //if (pnode != -1)
-                        //tempArr[j] = pnode;
-                }
-            }
-            std::cout << '\n';
-        }
-    }
-}
-
+#include "Graph.hpp"
+#include "Node.hpp"
 
 //This function reads in ordered pairs and populates our graph
 //Because we can't use the string library, I had to find some creative ways to make a sub-Cstring
 //It's kind of ugly but it works
-void readData(Graph<int>& G) {
-    G.initGraph(0);
+void readData(Graph<int>& G, std::ifstream &fin) {
     int node1 = 0;
     int node2 = 0;
     int comma = 0;
+    int initNum = 0;
     char fileline[10] = "6, 9";
+    char initiziler[10] = " ";
+    char cinit[10] = " ";
     //char nodes for sub-Cstrings
     char cnode1[10] = "   ";
     char cnode2[10] = "   ";
-
+    
     // strncpy (cnode1, fileline, sizeof(cnode1));
-
+    
     //read in from file
-    std::ifstream fin("data.txt");
+    try
+    {
+        fin.open("/Users/danielfishbein/Documents/computerProjects/Xcode/BFS/BFS/data.txt");
+        if (!fin)
+        {
+            throw;
+        }
+    }
+    catch (std::fstream::failure)
+    {
+        std::cerr << "Error with opening file on line 28" << std::endl;
+        throw;
+    }
+    //Grabs the total amount of vertices from the text file
+    fin.getline(initiziler, 100, '\n');
+    strncpy(cinit, initiziler, '\n');
+    initNum = atoi(cinit);
+    G.initGraph(initNum);
+    
     //store data in fileline
     while(fin.getline(fileline, 100)) {
         for(int i = 0; i < 10; i++) {
@@ -286,10 +50,10 @@ void readData(Graph<int>& G) {
                 break;
             }
         }
-
+        
         //Make a substring for the first node based on where the comma is
         strncpy(cnode1, fileline, comma);
-
+        
         //Convert that substring to an int
         node1 = atoi(cnode1);
         
@@ -301,26 +65,77 @@ void readData(Graph<int>& G) {
             }
             fileline[i] = ' ';
         }
-
+        
         //Make substring for second node
         strncpy(cnode2, fileline, 10);
-
+        
         //Convert substring to an int
         node2 = atoi(cnode2);
-
+        
         //std::cout << node1 << ", " << node2 << std::endl;
         G.createLink(node1, node2);
     }
+    fin.close();
 }
 
 int main(int argc, const char * argv[])
 {
-    Graph<int> G;
-    readData(G);
-    G.print(7, 32, 22, 3);
-    G.print(19, 32, 22, 3);
-    G.print(26, 32, 22, 3);
+    std::ofstream outf;
+    std::ifstream inf;
+    int src_o, src_t, src_th;
+    int dest_o, dest_t, dest_th;
+    
+    std::streambuf* stream_buffer_cerr = std::cerr.rdbuf();
+    std::streambuf* stream_buffer_outf = outf.rdbuf();
+    std::cerr.rdbuf(stream_buffer_outf);
+    
+    try
+    {
+        Graph<int> G;
+        if (argc != 2)
+            std::cout << "Usage of command line input is "  << argv[0] << "<filename>";
+        else
+        {
+            readData(G, inf);
+        }
+        
+        outf.open("errorlog.txt");
+        
+        if (!outf.is_open())
+            throw;
+        
+        std::cout << "Enter three sources:\n";
+        std::cin >> src_o >> src_t >> src_th;
+        if(src_o < 0 || src_t < 0 || src_th < 0)
+        {
+            throw std::invalid_argument("Negative value has been entered\n");
+        }
+        std::cout << "Enter three destinations:\n";
+        std::cin >> dest_o >> dest_t >> dest_th;
+        if (dest_o < 0 || dest_t < 0 || dest_th < 0)
+        {
+            throw std::invalid_argument("Negative value has been entered\n");
+        }
+        
+        G.print(src_o, dest_o, dest_t, dest_th);
+        G.print(src_t, dest_o, dest_t, dest_th);
+        G.print(src_th, dest_o, dest_t, dest_th);
+    }
+    catch (std::invalid_argument)
+    {
+        throw;
+    }
+    catch (std::fstream::failure)
+    {
+        std::cerr << "Error opening file check file path on line 102 in main" << std::endl;
+        throw;
+    }
+    catch (...)
+    {
+        std::cout << "Error with program operation. Terminating program, please reference error log file." << std::endl;
+        exit(1);
+    }
+    std::cerr.rdbuf(stream_buffer_cerr);
     
     return 0;
 }
-
